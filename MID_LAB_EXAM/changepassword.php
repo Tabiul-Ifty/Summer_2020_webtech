fbc19c5f2474fc95bf9097868167a98a8f6a997b
tabiulifty <tabiulifty@gmail.com>
2020-08-03 12:47:32 +0600

    2nd commit 12.45

---
+++ MID_LAB_EXAM/changepassword.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>change password</title>
+</head>
+<body>
+
+	<form action="" method="post">
+		<fieldset>
+			<legend>Change Password</legend>
+			<table>
+				<tr>
+					<td>Current Password</td></tr>
+
+				<tr>
+					<td><input type="password" name="CurrentPassword"></td>
+				</tr>
+				<tr>
+					<td>NewPassword</td></tr>
+
+				<tr>
+					<td><input type="password" name="NewPassword"></td>
+				</tr>
+				<tr>
+					<td>Retype New Password</td></tr>
+
+				<tr>
+					<td><input type="password" name="RnewPassword"></td>
+				</tr>
+
+				<tr>
+					<td><input type="submit" name="" value="Change"> <a href="homeAdmin.php">Home</a><br></td>
+				</tr>
+				
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
