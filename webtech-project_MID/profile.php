97c624fc41e5f733976f7190a9de16ddbf82ff02
tabiulifty <tabiulifty@gmail.com>
2020-08-09 15:40:47 +0600

    Mid Project Work progress 03

--- webtech-project_MID/profile.php
+++ webtech-project_MID/profile.php
@@ -12,7 +12,7 @@
 				<img src="Mt.jpg" width="50px" height="50px">
 			</td>
 
-				<td colspan="2" align="right">
+				<td colspan="3" align="right">
 				
 				<!--a href="profile.php">&ensp;Profile</a-->&ensp;
 				<a href="settings.php">Settings</a>&ensp;
@@ -26,29 +26,170 @@
 		</tr>
 
 		<tr height = "300px">
-			<td width = "300px">
+			<td >
 				<center><img src="profilepic.png" width="250px" height="250px" >
 				</center>
 			</td>
 
-			<td colspan="2">
+			<td colspan="3">
 				<div>
 					<h4>About Me!</h4>
 					<p>I like to play compitative games. I play ranked games like R6, PUBG and CSGO.</p>
 					<p></p>
-				</div>
-				
+				</div>	
 			</td>
+		</tr>
 
+				<tr height = "200px">
+			<td colspan="4">
+				<h2>Take your skill to the next level!</h2>
+				<center>
+					<h2>Games that you want to Master!</h2>
+					<form>
+						<select width = "150px">
 
+						<option>Players Unknown Battel Ground</option>
+						<option>Fortnite</option>
+						<option>DOTA2</option>
+						<option>League of Legendes</option>
+						<option>COD</option>
+						<option>CS:GO</option>
+					</select>
+					<br>
+					
+
+					<input type="submit" name="submit" value="Find Masters">
+					</form>
+					
+
+				</center>
+
+			</td>
 
 		</tr>
 
+		<tr height = "300px">
+			<td width= "25%">
+				<center>
+					<h3>Lessons</h3>
+					<br>
+					<hr>
+					<p>Calender View</p>
+					<hr>
+					<p>Purchase List</p>
+					<hr>
+					<p>My Reviews</p>
+				</center>
+			</td>
 
+			<td width= "25%">
+				<center>
+					<h3>Transaction</h3><br>
+					<hr>
+					<p>Wallet</p>
+					<hr>
+					<p>Purchase History</p>
+					<br>
+					<br>
+					<br>	
+				</center>
+			</td>
 
+			<td width= "25%">
+				
+				<center>
+					<h3>Achievements</h3>
+					<hr>
+					<p>View Achievments</p>
+					<br>
+					<br>
+					<br>
+					<br>
+				</center>
+			</td>
+			<td width= "25%">
+				<center>
+				    <h3>Games To Master</h3>
+					<hr>
+					<p>Wish List</p>
+					<br>
+					<br>
+					<br>
+					<br>
+				</center>
+			</td>
+		</tr>
 
+		<tr height = "300px">
+			<td width= "25%">
+				<center>
+					<h3>Profile information</h3>
+					<br>
+					<hr>
+					<p>Information View</p>
+					<br>
+					<br>
+					<br>
+					<br>
+				</center>
+					
+			</td>
+			<td width= "25%">
+				<center>
+					<h3>Methods Of learning</h3>
+					<br>
+					<hr>
+					<p>Discord</p>
+					<hr>
+					<p>Skype</p>
+					<br>
+					
+					
+				</center>
+				
+			</td>
 
+			<td width= "25%">
+				<center>
+					<h3>Upload Files</h3>
+					<br>
+					<hr>
+					<br>
 
+					<form method="post" enctype="multipart/form-data">
+			
+						File <input type="file" name="myfile">
+						<br>
+						<br>
+						<input type="submit" name="submit" value="Upload">
+			
+					</form>
+					<br>
+					<br>
+				
+					
+				</center>
+			</td>
+			<td width= "25%">
+			</td>
+		</tr>
+
+		<tr height = "250px">
+			<td>
+				<center><h4>Contact Information</h4></center>
+				<br>
+
+				<img src="gmail_50px.png" width="50px" height="50px"><p>abc@gmail.com</p>
+				<br>
+				<img src="phone_50px.png" width="50px" height="50px"><p>017**********</p>
+
+			</td>
+			<td colspan="3">
+				<center><h3>Privacy Policy</h3></center>
+				<p>Privacy policy of this website will be here</p>
+			</td>
+			
+		</tr>
 
 
 	</table>
