6ab8fc1f1a57c82b8149dc5adc7090302ad8c318
tabiulifty <tabiulifty@gmail.com>
2020-08-09 10:04:09 +0600

    Mid Project Wrok Progress 02

---
+++ webtech-project_MID/profile.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+</head>
+<body>
+
+	<table border="1" width="100%">
+
+		<tr height = "100px">
+			<td align="left">
+				<img src="Mt.jpg" width="50px" height="50px">
+			</td>
+
+				<td colspan="2" align="right">
+				
+				<!--a href="profile.php">&ensp;Profile</a-->&ensp;
+				<a href="settings.php">Settings</a>&ensp;
+				<a href="wallet.php">Wallet</a>&ensp;
+
+				<a href="https://www.w3schools.com">
+				<img border="0" src="question.png" width="30px" height="30px">
+
+				</td>
+
+		</tr>
+
+		<tr height = "300px">
+			<td width = "300px">
+				<center><img src="profilepic.png" width="250px" height="250px" >
+				</center>
+			</td>
+
+			<td colspan="2">
+				<div>
+					<h4>About Me!</h4>
+					<p>I like to play compitative games. I play ranked games like R6, PUBG and CSGO.</p>
+					<p></p>
+				</div>
+				
+			</td>
+
+
+
+		</tr>
+
+
+
+
+
+
+
+
+	</table>
+
+</body>
+</html>
